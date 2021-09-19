1720b62ee0155da1f046422021d2140081f96429
y4shiro <y4shironao@gmail.com>
2021-09-19 18:41:10 +0900

    axios で画像を取得

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,22 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions
   .region('asia-northeast1')
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
+    try {
+      const res = await axios.get('http://placehold.jp/150x150.png', {
+        responseType: 'arraybuffer',
+      });
+
+      functions.logger.info('get image', res.data);
+    } catch (err) {
+      functions.logger.info('failed get image');
+    }
+
     functions.logger.info('Hello logs!', { structuredData: true });
     response.send('Hello from Firebase!');
   });
