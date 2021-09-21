cd9f32495291be3514c19961caccbbbc69be9055
y4shiro <y4shironao@gmail.com>
2021-09-21 15:07:06 +0900

    Amdin SDK で取得した画像を Cloud Storage へ保存

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,16 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import axisos from 'axios';
 
 const config = functions.config().config;
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
+  storageBucket: config.storage_bucket,
 });
 
 const db = admin.firestore();
+const storage = admin.storage();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -16,15 +19,37 @@ export const helloWorld = functions
   .region('asia-northeast1')
   .https.onRequest(async (request, response) => {
     try {
-      const docRef = db.collection('users').doc('alovelace');
+      const imgRes = await axisos('http://placehold.jp/150x150.png', {
+        responseType: 'arraybuffer',
+      });
+
+      const docRef = db.collection('image').doc('png');
 
       await docRef.set({
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815,
+        header: imgRes.headers,
+        data: imgRes.data,
+        status: imgRes.status,
         env: config.env,
+        bucket: config.storage_bucket,
       });
       console.log('Document written with ID: ', docRef.id);
+
+      // Cloud Storage に書き込む
+      const blob = storage.bucket().file('test.png');
+      const blobStream = blob.createWriteStream({
+        resumable: false,
+        metadata: { 'Content-Type': 'image/png' },
+      });
+
+      blobStream.on('error', (err) => {
+        console.log(err);
+      });
+
+      blobStream.on('finish', () => {
+        console.log('image upload!');
+      });
+
+      blobStream.end(imgRes.data);
     } catch (e) {
       console.error('Error adding document: ', e);
     }
